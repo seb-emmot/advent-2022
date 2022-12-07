3249a9b23aeb316eec68ad48cedc9ff81415bded
Sebastian Emmot <sebastian.emmot@gmail.com>
2022-12-07 17:22:50 +0100

    Rename day1.go to main.go
    Preparation for day2 task

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func check(e error) {
 	}
 }
 
-func main() {
+func day1() {
 	readFile, err := os.Open("input.txt")
 	check(err)
 
@@ -58,4 +58,8 @@ func main() {
 	fmt.Println("total", sum)
 
 	readFile.Close()
+}
+
+func main() {
+	day1()
 }
